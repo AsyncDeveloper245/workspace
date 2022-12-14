cbb2b94c4b09916aae4058ae6f95b6b1e60c10aa
Omoleye Julius Fortunate <79061404+AsyncDeveloper245@users.noreply.github.com>
2022-12-14 15:54:32 +0100

    Update molotov_load_test.py

--- molotov_load_test.py
+++ molotov_load_test.py
@@ -7,3 +7,7 @@ import molotov
 async def scenario_one(session):
    async with session.get("http://localhost:5000") as resp:
    assert resp.status == 200
+
+   
+#Run this scriot in the command line with the command below
+# molotov -v -r 100 molotov_load_test.py
