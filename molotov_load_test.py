66b5e4f3e124c5b61f78f41441419ce0935951cc
Omoleye Julius Fortunate <79061404+AsyncDeveloper245@users.noreply.github.com>
2022-12-14 15:57:11 +0100

    Update molotov_load_test.py

--- molotov_load_test.py
+++ molotov_load_test.py
@@ -3,11 +3,18 @@
 import molotov
 
 
-@molotov.scenario(100)
+@molotov.scenario(30)
 async def scenario_one(session):
    async with session.get("http://localhost:5000") as resp:
    assert resp.status == 200
 
+   
+@molotov.scenario(70)
+async def scenario_post(session):
+    resp = await session.post("http://localhost:5000", params={'q': 'devops'})
+    redirect_status = resp.history[0].status
+    error = "unexpected redirect status: %s" % redirect_status
+    assert redirect_status == 301, error
    
 #Run this scriot in the command line with the command below
 # molotov -v -r 100 molotov_load_test.py
